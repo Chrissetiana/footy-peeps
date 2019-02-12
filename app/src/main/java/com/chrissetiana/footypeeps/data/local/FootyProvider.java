b2692ca2e8d0d33e64ab7e78ed6003841c8855dc
Tiana Aiyemo <chrissetiana@gmail.com>
2019-02-12 16:52:08 +0100

    Add: local data classes

---
+++ app/src/main/java/com/chrissetiana/footypeeps/data/local/FootyProvider.java
@@ -0,0 +1,63 @@
+package com.chrissetiana.footypeeps.data.local;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+public class FootyProvider extends ContentProvider {
+
+    private FootyHelper helper;
+    private static final int TABLE_ID = 0;
+    private static final int ROW_ID = 1;
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        uriMatcher.addURI(FootyContract.AUTHORITY, FootyContract.PATH, TABLE_ID);
+        uriMatcher.addURI(FootyContract.AUTHORITY, FootyContract.PATH + "/#", ROW_ID);
+    }
+
+    public FootyProvider() {
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        // TODO: Implement this to handle requests to insert a new row.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onCreate() {
+        // TODO: Implement this to initialize your content provider on startup.
+        return false;
+    }
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
